dd15725d2fe7fe69c9002889e2a1f7b9fc3856ff
ParnThanatibordee <parn8946@gmail.com>
2023-02-02 00:54:46 +0700

    deploying our database

--- working/scripts/providers.tf
+++ working/scripts/providers.tf
@@ -2,8 +2,17 @@
 
 provider "azurerm" {
     version = "2.92.0"
+}
 
-    features {
-        
-    }
+provider "tls" {
+    version = "4.0.4"
+}
+
+provider "kubernetes" {
+    version = "1.10.0"
+    host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+    client_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
 }
